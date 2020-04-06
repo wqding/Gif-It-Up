8f5fa47a41ec2e908021d00837913e919603141e
Adam Ding <wqding@uwaterloo.ca>
2020-04-06 19:04:26 -0400

    created server but not needed

---
+++ src/server/server.js
@@ -0,0 +1,13 @@
+// I realized you don't even need a server
+
+// const express = require('express');
+// const axios = require('axios');
+// const bodyParser = require('body-parser');
+
+// const app = express();
+// const port = 5000;
+
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+
+// app.listen(port, () => console.log(`Listening on port ${port}`));
