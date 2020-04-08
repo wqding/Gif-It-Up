f290e00aa373f1ce98bd16a94acd89cef4711f06
Adam Ding <wqding@uwaterloo.ca>
2020-04-08 00:14:19 -0400

    added loading animation

--- src/components/SearchGifs.js
+++ src/components/SearchGifs.js
@@ -4,22 +4,29 @@ import {Jumbotron} from 'react-bootstrap'
 import SearchBar from './SearchBar'
 import TileList from './TileList';
 import GifDialog from './GifDialog'
+import Backdrop from '@material-ui/core/Backdrop';
+import CircularProgress from '@material-ui/core/CircularProgress';
+
 
 import {SearchResultsContext} from '../contexts/SearchResultsContext'
 
 
 const SearchGifs = () => {
     const [results, setResults] = useContext(SearchResultsContext);
+    const [openLoading, setOpenLoading] = useState(false);
 
     return(
         <div>
             <Jumbotron fluid>
                 <Container>
-                    <SearchBar/>
+                    <SearchBar setOpenLoading={setOpenLoading}/>
                 </Container>
             </Jumbotron>
             <TileList gifs={results}/>
             <GifDialog/>
+            <Backdrop open={openLoading} style={{zIndex: 2}}>
+                <CircularProgress color="inherit" />
+            </Backdrop>
         </div>
     )
 }
