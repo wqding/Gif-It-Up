af0a12f6ec152c6d39d27a5b0b26eb16d37ca7d8
Adam Ding <wqding@uwaterloo.ca>
2020-04-08 00:13:58 -0400

    implemented random, translate, and trending

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,6 +3,8 @@ import IconButton from '@material-ui/core/IconButton';
 import SearchIcon from '@material-ui/icons/Search';
 import { Paper, InputBase, Button } from '@material-ui/core';
 import {SearchResultsContext} from '../contexts/SearchResultsContext'
+import {GifDialogContext} from '../contexts/GifDialogContext'
+
 import  { Link } from 'react-router-dom'
 
 import axios from 'axios';
@@ -11,19 +13,61 @@ import axios from 'axios';
 const SEARCH_NAME_PARAM ='q';
 const TRANSLATE_NAME_PARAM = 's';
 
-const SearchBar = () => {
-    const [searchText, setSearchText] = useState();
+const SearchBar = (props) => {
+    const [inputText, setInputText] = useState();
     const [results, setResults] = useContext(SearchResultsContext);
+    const [gifShown, setGifShown] = useContext(GifDialogContext);
+    const [showGifDialog, setShowGifDialog] = useContext(GifDialogContext);
 
-    const searchGifs = () =>{
-        let url = `https://knowhow-react-challenge.prod.with-datafire.io/gif/search?${SEARCH_NAME_PARAM}=${searchText}`;
+    const showGif = (gif) => {
+        setShowGifDialog(true);
+        setGifShown(gif);
+    }
+
+    const loadMultipleGifsFromURL = (url) => {
+        props.setOpenLoading(true);
         axios.get(url)
         .then(res => {
             storeSearchResults(res.data)
+            props.setOpenLoading(false);
         })
         .catch(err => {
             console.log(err)
         });
+    }
+
+    const loadSingleGifFromURL = (url) => {
+        props.setOpenLoading(true);
+        axios.get(url)
+        .then(res => {
+            let gif = {
+                id: res.data.data.id,
+                saved: false,
+                title:res.data.data.title,
+                username: res.data.data.username,
+                src: res.data.data.images.downsized.url
+            }
+            showGif(gif)
+            props.setOpenLoading(false);
+        })
+        .catch(err => {
+            console.log(err)
+        });
+    }
+
+    const searchGifs = () =>{
+        let url = `https://knowhow-react-challenge.prod.with-datafire.io/gif/search?${SEARCH_NAME_PARAM}=${inputText}`;
+        loadMultipleGifsFromURL(url);
+    }
+
+    const showRandom = () => {
+        let url = `https://knowhow-react-challenge.prod.with-datafire.io/gif/random`;
+        loadSingleGifFromURL(url);
+    }
+
+    const showTrending = () => {
+        let url = `https://knowhow-react-challenge.prod.with-datafire.io/gif/trending`;
+        loadMultipleGifsFromURL(url);
     }
 
     const storeSearchResults = (searchResults) => {
@@ -39,23 +83,26 @@ const SearchBar = () => {
         setResults(condensedResults);
     }
 
-    const showRandomGif = () => {
-        
+    const showTranslate = () => {
+        let url = `https://knowhow-react-challenge.prod.with-datafire.io/gif/translate?${TRANSLATE_NAME_PARAM}=${inputText}`;
+        loadSingleGifFromURL(url);
     }
     
     return(
         <Paper>
             <InputBase
+                style={{marginLeft:10}}
                 autoFocus
-                className='search-input'
                 placeholder="Search"
-                onChange={(e) => {setSearchText(e.target.value)}}
+                onChange={(e) => {setInputText(e.target.value)}}
             />
             <IconButton color="primary" aria-label="search" onClick={searchGifs}>
                 <SearchIcon />
             </IconButton>
+            <Button onClick={showTranslate} color="primary">Translate</Button>
+            <Button onClick={showRandom} color="primary">Random</Button>
+            <Button onClick={showTrending} color="primary">Trending</Button>
             <Link to='/saved'><Button color="primary">View Saved</Button></Link>
-            <Button onClick={showRandomGif} color="primary">Random</Button>
         </Paper>
     )
 }
